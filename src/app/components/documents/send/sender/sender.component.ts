6f514689303acdd877fa48514c72f5712acc2d9d
Mateusz <Onomatopeja1342>
2019-05-22 09:39:54 +0200

    add loading spiner

--- src/app/components/documents/send/sender/sender.component.ts
+++ src/app/components/documents/send/sender/sender.component.ts
@@ -17,6 +17,7 @@ export class SenderComponent implements OnInit, OnDestroy {
   public enabledValidators: boolean;
   public documentTypes: SelectItem[];
   private differ: any;
+  private loading;
 
   constructor(private formBuilder: FormBuilder,
               private authService: AuthService,
@@ -25,6 +26,7 @@ export class SenderComponent implements OnInit, OnDestroy {
               private messageService: MessageService,
               private iterableDiffers: IterableDiffers,
               private documentService: DocumentService) {
+    this.loading = false;
     this.enabledValidators = false;
     this.documentTypes = [
       {label: 'Wybierz Rodzaj', value: null},
@@ -43,15 +45,17 @@ export class SenderComponent implements OnInit, OnDestroy {
     if (this.uploadService.files.length) {
       this.enabledValidators = true;
       if (!this.sendForm.invalid) {
+        this.loading = true;
         this.uploadService.startUpload(this.uploadService.files)
             .then((files: any) => {
               value.documents = files;
               this.documentService.sendCase(value).subscribe(() => {
                 this.router.navigate(['documents/view']);
-              });
+              }, () => this.loading = false);
             })
             .catch((error) => {
               console.log(`Some failed: `, error.message);
+              this.loading = false;
             });
       }
     } else {

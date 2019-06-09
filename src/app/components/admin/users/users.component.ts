b7a75a81974fdb45e38041b04404d71e4f845c29
xantimage@gmail.com <Ia9uquf2!>
2019-06-09 16:15:02 +0200

    user component hotfix

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminService} from '../../../services/admin.service';
+import {User} from '../../../models/user';
 import {MessageService} from 'primeng/api';
 
 @Component({
